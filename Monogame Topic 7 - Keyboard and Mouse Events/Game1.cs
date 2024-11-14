b305e5164d1a4fa9026898b94191efc09a573e3e
Jack Gallagher <339690067@GoTVDSB.ca>
2024-11-14 09:38:01 -0500

    Class Ended

--- Monogame Topic 7 - Keyboard and Mouse Events/Game1.cs
+++ Monogame Topic 7 - Keyboard and Mouse Events/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
 {
@@ -8,6 +9,19 @@ namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Texture2D pacDownTexture, pacUpTexture, pacLeftTexture, pacRightTexture, pacSleepTexture;
+        Rectangle pacLocation;
+
+        SpriteFont instructionFont;
+
+        KeyboardState keyboardState, oldKeyboardState;
+
+        MouseState mouseState;
+
+        Random generator = new Random();
 
         public Game1()
         {
@@ -19,6 +33,12 @@ namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            window = new Rectangle(0, 0, 800, 600);
+            pacLocation = new Rectangle(10, 10, 75, 75);
+
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.ApplyChanges();
 
             base.Initialize();
         }
@@ -28,6 +48,12 @@ namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacSleepTexture = Content.Load<Texture2D>("PacSleep");
+            instructionFont = Content.Load<SpriteFont>("Instructions");
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +62,56 @@ namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
                 Exit();
 
             // TODO: Add your update logic here
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                pacLocation.Y -= 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                pacLocation.Y += 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.A))
+            {
+                pacLocation.X -= 2;
+            }
+            if (keyboardState.IsKeyDown(Keys.D))
+            {
+                pacLocation.X += 2;
+            }
+
+            if (oldKeyboardState.IsKeyUp(Keys.Space) && keyboardState.IsKeyDown(Keys.Space))
+            {
+                pacLocation.X = generator.Next(0, 801);
+                pacLocation.Y = generator.Next(0, 601);
+            }
+
+            if (pacLocation.Left >= window.Width)
+            {
+                pacLocation.X = -74;
+            }
+            if (pacLocation.Right <= 0)
+            {
+                pacLocation.X = window.Width;
+            }
+            if (pacLocation.Top >= window.Height)
+            {
+                pacLocation.Y = -74;
+            }
+            if (pacLocation.Bottom <= 0)
+            {
+                pacLocation.Y = window.Height;
+            }
+
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                pacLocation.X = mouseState.X - 37;
+                pacLocation.Y = mouseState.Y - 37;
+            }
+
+            oldKeyboardState = keyboardState;
 
             base.Update(gameTime);
         }
@@ -45,6 +121,31 @@ namespace Monogame_Topic_7___Keyboard_and_Mouse_Events
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                _spriteBatch.Draw(pacUpTexture, pacLocation, Color.White);
+            }
+            else if (keyboardState.IsKeyDown(Keys.S))
+            {
+                _spriteBatch.Draw(pacDownTexture, pacLocation, Color.White);
+            }
+            else if (keyboardState.IsKeyDown(Keys.A))
+            {
+                _spriteBatch.Draw(pacLeftTexture, pacLocation, Color.White);
+            }
+            else if (keyboardState.IsKeyDown(Keys.D))
+            {
+                _spriteBatch.Draw(pacRightTexture, pacLocation, Color.White);
+            }
+            else
+            {
+                _spriteBatch.Draw(pacSleepTexture, pacLocation, Color.White);
+            }
+
+            _spriteBatch.DrawString(instructionFont, "Use WASD to move", new Vector2(0, 0), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
